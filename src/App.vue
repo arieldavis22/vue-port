98915d0cdedfa121d9e25729a39630e4490253e4
Ariel Davis <amanichai100@gmail.com>
2023-03-11 21:11:23 -0500

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,48 @@
+
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <a v-for="(link, index) in navLinks" :key="index" :href="link.id" @click.prevent="scrollTo(link.id)">{{ link.text }} | </a>
   </nav>
-  <router-view/>
+  <HomeView id="home"/>
+  <ProjectsView id="projects"/>
+  <ContactView id="contact" />
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+import ContactView from './views/ContactView.vue';
+import HomeView from './views/HomeView.vue';
+import ProjectsView from './views/ProjectsView.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {HomeView, ProjectsView, ContactView},
+  setup() {
+    const navLinks = ref([
+      {
+        id: "#home",
+        text: "Home"
+      },
+      {
+        id: "#projects",
+        text: "Projects"
+      },
+      {
+        id: "#contact",
+        text: "Contact"
+      },
+    ])
+
+    const scrollTo = (selector: string) => {
+      const aTag = document.querySelector<HTMLAnchorElement>(selector)
+      if(aTag) aTag.scrollIntoView({behavior: 'smooth'})
+    }
+    
+    return {navLinks, scrollTo}
+  }
+});
+</script>
 
 <style>
 #app {
@@ -22,9 +60,5 @@ nav {
 nav a {
   font-weight: bold;
   color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div style="height: 100vh; border-bottom: 1px red solid;">
+    projects
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
